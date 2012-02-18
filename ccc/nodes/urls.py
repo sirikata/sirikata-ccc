ccac440f6ce4ae8edd8f12694b2dfd0855a3e596
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2012-02-17 20:32:26 -0800

    Add a URL handler for raw commands.
    Add a URL handler for raw commands that just forwards commands (with
    no additional parameters) to a node. For example, using the URL
    /nodes/node/1/command/raw/space.server.objects.count would execute the
    command 'space.server.objects.count' on the server and report the raw
    result back. This is proof of concept for interacting with
    nodes. These URLs aren't linked anywhere; you have to enter commands
    manually.

--- ccc/nodes/urls.py
+++ ccc/nodes/urls.py
@@ -6,6 +6,10 @@ urlpatterns = patterns(
     url(r'^$', 'nodes.views.index', name='ccc-nodes-index'),
 
     url(r'^node/(?P<node_id>\d+)/$', 'nodes.views.node', name='ccc-nodes-node-index'),
+
+    # Raw command to the node. Just requests the command_name and returns the raw result
+    url(r'^node/(?P<node_id>\d+)/command/raw/(?P<command_name>(\w|\.)+)$', 'nodes.views.node_command_raw', name='ccc-nodes-node-command-raw'),
+
     url(r'^group/(?P<group_id>\d+)/$', 'nodes.views.group', name='ccc-nodes-group-index'),
 
 )
--- ccc/nodes/views.py
+++ ccc/nodes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from nodes.models import Node, NodeGroup
+import urllib2
 
 def index(request):
     nodes = Node.objects.all().order_by('name')
@@ -14,6 +15,22 @@ def node(request, node_id):
         'node' : node
         }
     return render_to_response('node.html', render_params)
+
+
+def node_command_raw(request, node_id, command_name):
+    node = get_object_or_404(Node, pk=node_id)
+
+    try:
+        url_response = urllib2.urlopen('http://' + node.address + '/' + command_name)
+        response = url_response.read()
+    except urllib2.URLError:
+        response = 'Error when issuing command to node'
+
+    render_params = {
+        'response' : response
+        }
+    return render_to_response('raw.html', render_params)
+
 
 def group(request, group_id):
     group = get_object_or_404(NodeGroup, pk=group_id)
