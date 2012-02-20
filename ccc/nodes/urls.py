660852775e1ac9d328781135284523acad1726ff
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2012-02-20 00:40:23 -0800

    Add object sessions page with disconnect buttons.
    Add object sessions page and link to it from the node info
    page. Refactor the request made in the raw command page to be a
    reusable method for issuing commands and add support for POST data as
    well as the command name. Add a disconnect URL handler which just
    makes the disconnection request and redirects to the sessions page.

--- ccc/nodes/urls.py
+++ ccc/nodes/urls.py
@@ -10,6 +10,10 @@ urlpatterns = patterns(
     # Raw command to the node. Just requests the command_name and returns the raw result
     url(r'^node/(?P<node_id>\d+)/command/raw/(?P<command_name>(\w|\.)+)$', 'nodes.views.node_command_raw', name='ccc-nodes-node-command-raw'),
 
+    # Objects on a given node
+    url(r'^node/(?P<node_id>\d+)/objects/$', 'nodes.views.node_objects', name='ccc-nodes-node-objects'),
+    url(r'^node/(?P<node_id>\d+)/objects/disconnect/(?P<obj_id>(\d|\w|-)+)$', 'nodes.views.node_disconnect_object', name='ccc-nodes-node-objects-disconnect'),
+
     url(r'^group/(?P<group_id>\d+)/$', 'nodes.views.group', name='ccc-nodes-group-index'),
 
 )
--- ccc/nodes/views.py
+++ ccc/nodes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 from nodes.models import Node, NodeGroup
 import requests
+import json
 
 def index(request):
     nodes = Node.objects.all().order_by('name')
@@ -24,14 +25,31 @@ def node(request, node_id):
         )
 
 
-def node_command_raw(request, node_id, command_name):
-    node = get_object_or_404(Node, pk=node_id)
+def run_node_command(node, command_name, command_body=None, raw=False):
+    '''
+    Run a node command and get the result, optionally as a raw string
+    instead of parsed.
+    '''
+
+    if command_body:
+        command_body = json.dumps(command_body)
 
     try:
-        r = requests.get('http://' + node.address + '/' + command_name)
+        r = requests.post('http://' + node.address + '/' + command_name, data=command_body)
         response = r.text
     except requests.exceptions.RequestException as e:
         response = 'Error issuing command to node: ' + str(e)
+
+    if not raw:
+        response = json.loads(response)
+
+    return response
+
+
+def node_command_raw(request, node_id, command_name):
+    node = get_object_or_404(Node, pk=node_id)
+
+    response = run_node_command(node, command_name, raw=True)
 
     render_params = {
         'response' : response
@@ -41,6 +59,32 @@ def node_command_raw(request, node_id, command_name):
         context_instance=RequestContext(request)
         )
 
+
+def node_objects(request, node_id):
+    node = get_object_or_404(Node, pk=node_id)
+
+    response = run_node_command(node, "space.server.objects.list")
+
+    objects = []
+    if 'objects' in response:
+        objects = response['objects'].keys()
+
+    render_params = {
+        'node' : node,
+        'objects' : objects
+        }
+    return render_to_response(
+        'objects.html', render_params,
+        context_instance=RequestContext(request)
+        )
+
+def node_disconnect_object(request, node_id, obj_id):
+    node = get_object_or_404(Node, pk=node_id)
+
+    response = run_node_command(node, "space.server.objects.disconnect", { 'object' : obj_id })
+
+    # Use a notification to indicate success after redirect?
+    return redirect('ccc-nodes-node-objects', node_id=node_id)
 
 def group(request, group_id):
     group = get_object_or_404(NodeGroup, pk=group_id)
