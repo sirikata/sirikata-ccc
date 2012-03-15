a106f033562ce75d008bbc8143ee8520987669db
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2012-03-15 13:44:06 -0700

    Add a nicer error page when node commands fail.

--- ccc/nodes/views.py
+++ ccc/nodes/views.py
@@ -34,28 +34,42 @@ def run_node_command(node, command_name, command_body=None, raw=False):
     if command_body:
         command_body = json.dumps(command_body)
 
-    error = False
+    error = None
     try:
         r = requests.post('http://' + node.address + '/' + command_name, data=command_body)
         response = r.text
     except requests.exceptions.RequestException as e:
-        response = 'Error issuing command to node: ' + str(e)
-        error = True
+        error = str(e)
+        return None, error
 
-    if not raw and not error:
+    if not raw:
         # Try to parse, but fallback to original value if we can't
         try:
             response = json.loads(response)
         except ValueError:
             pass
 
-    return response
+    return response, error
+
+
+def failed_command(request, message):
+    '''Returns an error page'''
+
+    render_params = {
+        'message' : message
+        }
+    return render_to_response(
+        'command_error.html', render_params,
+        context_instance=RequestContext(request)
+        )
 
 
 def node_command_raw(request, node_id, command_name):
     node = get_object_or_404(Node, pk=node_id)
 
-    response = run_node_command(node, command_name, raw=True)
+    response, error = run_node_command(node, command_name, raw=True)
+    if not response: return failed_command(request, error)
+
     # Reformat if we get back json
     try:
         resp_json = json.loads(response)
@@ -80,7 +94,9 @@ def node_list_commands(request, node_id):
     '''
     node = get_object_or_404(Node, pk=node_id)
 
-    response = run_node_command(node, 'meta.commands')
+    response, error = run_node_command(node, 'meta.commands')
+    if not response: return failed_command(request, error)
+
     commands = []
     if 'commands' in response:
         commands = [c for c in response['commands']]
@@ -99,7 +115,8 @@ def node_list_commands(request, node_id):
 def node_objects(request, node_id):
     node = get_object_or_404(Node, pk=node_id)
 
-    response = run_node_command(node, "space.server.objects.list")
+    response, error = run_node_command(node, "space.server.objects.list")
+    if not response: return failed_command(request, error)
 
     objects = []
     if 'objects' in response:
@@ -117,7 +134,8 @@ def node_objects(request, node_id):
 def node_disconnect_object(request, node_id, obj_id):
     node = get_object_or_404(Node, pk=node_id)
 
-    response = run_node_command(node, "space.server.objects.disconnect", { 'object' : obj_id })
+    response, error = run_node_command(node, "space.server.objects.disconnect", { 'object' : obj_id })
+    if not response: return failed_command(request, error)
 
     # Use a notification to indicate success after redirect?
     return redirect('ccc-nodes-node-objects', node_id=node_id)
@@ -127,7 +145,8 @@ def node_disconnect_object(request, node_id, obj_id):
 def node_transfer_requests(request, node_id):
     node = get_object_or_404(Node, pk=node_id)
 
-    response = run_node_command(node, "transfer.mediator.requests.list")
+    response, error = run_node_command(node, "transfer.mediator.requests.list")
+    if not response: return failed_command(request, error)
 
     requests = []
     if 'requests' in response:
@@ -149,7 +168,8 @@ def node_debug(request, node_id):
     '''
     node = get_object_or_404(Node, pk=node_id)
 
-    context_stats = run_node_command(node, "context.report-all-stats")
+    context_stats, error = run_node_command(node, "context.report-all-stats")
+    if not response: return failed_command(request, error)
 
     render_params = {
         'node' : node,
